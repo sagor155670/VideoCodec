ce0dec1860822c3c6a8986be39c407e24a4eb761
sagor155670 <sagor@braincraftapps.com>
2023-11-21 13:15:11 +0600

    Initial Commit

---
+++ VideoCodec/VideoCodecApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCodecApp.swift
+//  VideoCodec
+//
+//  Created by Saiful Islam Sagor on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoCodecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
